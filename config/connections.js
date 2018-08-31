80a4c5df09868fd5a262efbbd555b357fc5ecb05
Needy <christian139601@gmail.com>
2018-08-31 12:25:16 +0300

    hide more sensitive data

--- config/connections.js
+++ config/connections.js
@@ -37,15 +37,15 @@ module.exports.connections = {
       "project_id": process.env.firebase_project_id,
       "private_key_id": process.env.firebase_private_key_id,
       "private_key": JSON.parse(process.env.firebase_private_key),
-      "client_email": process.env.client_email,
-      "client_id": process.env.client_id,
+      "client_email": process.env.firebase_client_email,
+      "client_id": process.env.firebase_client_id,
       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
       "token_uri": "https://oauth2.googleapis.com/token",
       "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": process.env.client_x509_cert_url
+      "client_x509_cert_url": process.env.firebase_client_x509_cert_url
     },
 
-    databaseURL: "https://restaurant-reviews-bc837.firebaseio.com",
+    databaseURL: process.env.firebase_databaseURL,
   },
 
   /***************************************************************************
