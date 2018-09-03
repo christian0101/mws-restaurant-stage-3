2da0896cd789da71d85867a43a2700f01ca7a450
Needy <christian139601@gmail.com>
2018-09-03 17:06:30 +0300

    test

--- server.js
+++ server.js
@@ -24,6 +24,8 @@
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
 
+console.log(process.env.PORT);
+
 // Attempt to import `sails`.
 var sails;
 try {
