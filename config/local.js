9867cee808de237322db525ac8c433aab55317d6
Needy <christian139601@gmail.com>
2018-09-03 16:46:32 +0300

    feat: local config

---
+++ config/local.js
@@ -0,0 +1,12 @@
+// Local configuration
+//
+// Included in the .gitignore by default,
+// this is where you include configuration overrides for your local system
+// or for a production deployment.
+//
+// For example, to use port 80 on the local machine, override the `port` config
+
+module.exports = {
+    port: 3003,
+    environment: 'production'
+}
