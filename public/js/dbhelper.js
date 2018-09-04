76d282ff1cfc6752c2c68441159edd82e05e4af6
Needy <christian139601@gmail.com>
2018-09-04 20:36:23 +0300

    use https

--- public/js/dbhelper.js
+++ public/js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     //const port = 1337 // Change this to your server port
-    return `//api-restaurant-reviews.herokuapp.com`;
+    return `https://api-restaurant-reviews.herokuapp.com`;
   }
 
   static get errorMsg() {
